8dc663a00896bc597787a86136f1764c46c3b53d
melvinjoseph303 <82402414+melvinjoseph303@users.noreply.github.com>
2021-06-12 11:59:49 +0530

    Update authorRoutes.js

--- src/routes/authorRoutes.js
+++ src/routes/authorRoutes.js
@@ -1,46 +1,60 @@
 const express = require('express');
+const Authordata = require('../model/Authordata');
 const authorsRouter = express.Router();
 function router(nav){
-var authors = [
-    {
-       name:'Joseph Barbera',
-       age:'60',
-       type:'cartoon artist',
-       img:'JosephBarbera.jpg'
-    },
-    {
-       name:'J K Rowling',
-       age:'55',
-       type:'fantasy',
-       img:'JKRowling.jpg'
-    },
-    {
-       name:'Basheer',
-       age:'Basheer',
-       type:'novelist',
-       img:'Basheer1.jpg'
-    }
- ]
+// var authors = [
+//     {
+//        name:'Joseph Barbera',
+//        age:'60',
+//        type:'cartoon artist',
+//        img:'JosephBarbera.jpg'
+//     },
+//     {
+//        name:'J K Rowling',
+//        age:'55',
+//        type:'fantasy',
+//        img:'JKRowling.jpg'
+//     },
+//     {
+//        name:'Basheer',
+//        age:'Basheer',
+//        type:'novelist',
+//        img:'Basheer1.jpg'
+//     }
+//  ]
  
  authorsRouter.get('/',function(req,res){
     // res.sendFile(__dirname+"/src/views/index.html");
-    res.render("authors",
-    {
-       nav,
-       title:'Library',
-       authors
-    });
+    Authordata.find()
+    .then(function(authors){
+      res.render("authors",
+      {
+         nav,
+         title:'Library',
+         authors
+      });
+    })
+    
  });
  
  authorsRouter.get('/:id',function(req,res){
     const id = req.params.id
-    res.render("author",
-    {
-       nav,
-       title:'Library',
-       author: authors[id]
-    });
+    Authordata.findOne({_id:id})
+    .then(function(author){
+      res.render("author",
+      {
+         nav,
+         title:'Library',
+         author
+      });
+    })
+    
  });
+
+
+
+
+
  return authorsRouter;
 }
 module.exports = router;
