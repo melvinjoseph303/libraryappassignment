f13fdc6cff69c4495870be6662f42dba256d9eab
melvin <melvinjoseph303@gmail.com>
2021-06-12 10:39:12 +0530

    First Commit: First Version

--- src/model/Authordata.js
+++ src/model/Authordata.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//mongoose.connect('mongodb://localhost:27017/library');
-mongoose.connect('mongodb+srv://userone:userone@ictakfiles.1k3dv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb://localhost:27017/library');
+//mongoose.connect('mongodb+srv://userone:userone@ictakfiles.1k3dv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 const AuthorSchema = new Schema({
         name: String,
