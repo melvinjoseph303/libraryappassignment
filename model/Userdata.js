a6ee9f215217370ab53562402bb2e30928ebcb42
melvinjoseph303 <82402414+melvinjoseph303@users.noreply.github.com>
2021-06-12 13:13:41 +0530

    Add files via upload

---
+++ model/Userdata.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
+mongoose.connect('mongodb://localhost:27017/library');
+//mongoose.connect('mongodb+srv://userone:userone@ictakfiles.1k3dv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+        username: String,
+        password: String,
+        phoneno: Number
+});
+UserSchema.plugin(passportLocalMongoose);
+var Userdata = mongoose.model('userdata',UserSchema);
+module.exports = Userdata;
