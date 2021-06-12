9c70b2ec1b924a1962ade8adc65d52a7ff4569fc
melvinjoseph303 <82402414+melvinjoseph303@users.noreply.github.com>
2021-06-12 12:03:02 +0530

    Update bookRoutes.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,46 +1,59 @@
 const express = require('express');
+const Bookdata = require('../model/Bookdata');
 const booksRouter = express.Router();
 function router(nav){
-var books = [
-    {
-       title:'Tom And Jerry',
-       author:'Joseph Barbera',
-       genre:'cartoon',
-       img:'TomandJerry.jpg'
-    },
-    {
-       title:'Harry Potter',
-       author:'J K Rowling',
-       genre:'fantasy',
-       img:'harrypotter.jpg'
-    },
-    {
-       title:'Paathummayude Aadu',
-       author:'Basheer',
-       genre:'drama',
-       img:'Basheer.jpg'
-    }
- ]
+// var books = [
+//     {
+//        title:'Tom And Jerry',
+//        author:'Joseph Barbera',
+//        genre:'cartoon',
+//        img:'TomandJerry.jpg'
+//     },
+//     {
+//        title:'Harry Potter',
+//        author:'J K Rowling',
+//        genre:'fantasy',
+//        img:'harrypotter.jpg'
+//     },
+//     {
+//        title:'Paathummayude Aadu',
+//        author:'Basheer',
+//        genre:'drama',
+//        img:'Basheer.jpg'
+//     }
+//  ]
  
  booksRouter.get('/',function(req,res){
     // res.sendFile(__dirname+"/src/views/index.html");
-    res.render("books",
-    {
-       nav,
-       title:'Library',
-       books
-    });
+    Bookdata.find()
+    .then(function(books){
+      res.render("books",
+      {
+         nav,
+         title:'Library',
+         books
+      });
+    })
+    
  });
  
  booksRouter.get('/:id',function(req,res){
-    const id = req.params.id
-    res.render("book",
-    {
-       nav,
-       title:'Library',
-       book: books[id]
-    });
+    const id = req.params.id;
+    Bookdata.findOne({_id:id})
+    .then(function(book){
+      res.render("book",
+      {
+         nav,
+         title:'Library',
+         book
+      });
+    })
+    
  });
+
+
+ 
+
  return booksRouter;
 }
 module.exports = router;
